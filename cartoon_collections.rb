b41963099f0a2a91fc52e8d0b88337c330c856bf
Michael Schneider <mic.sch91@gmail.com>
2020-06-04 14:53:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,4 +24,3 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find { |call| valid_calls.include?(call) }
 end
-
